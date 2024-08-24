a2d0132592f38ba97656d2269fa56c72832f2e62
Francisco Curiel de Icaza <ciclosporina2@hotmail.com>
2024-08-24 15:54:16 -0400

    Update openai_setup.py

--- gpt_config/openai_setup.py
+++ gpt_config/openai_setup.py
@@ -1,21 +1,12 @@
 # gpt_config/openai_setup.py
 import openai
 import streamlit as st
-import logging
-
-# Configure logging
-logging.basicConfig(level=logging.INFO)
 
 def initialize_openai():
-    """Inicializa OpenAI y obtiene la clave API de los secretos de Streamlit."""
-    OPENAI_API_KEY = st.secrets.get("openai", {}).get("api_key", None)
-
+    """Inicializa OpenAI y obtiene la clave API de Streamlit Secrets."""
+    OPENAI_API_KEY = st.secrets["OPENAI_API_KEY"]
     if not OPENAI_API_KEY:
-        st.error("Please add your OpenAI API key to the Streamlit secrets.")
+        st.error("Please add your OpenAI API key to the Streamlit secrets.toml file.")
         st.stop()
-
-    # Asignar la clave API de OpenAI
     openai.api_key = OPENAI_API_KEY
-    logging.info("OpenAI API Key successfully retrieved from Streamlit secrets.")
-    
-    return openai
+    return openai.OpenAI()
