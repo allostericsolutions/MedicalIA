0fdcb5b95cd14a9ec192c190561fe48407ceb220
Francisco Curiel de Icaza <ciclosporina2@hotmail.com>
2024-08-24 18:51:56 -0400

    Update openai_setup.py

--- gpt_config/openai_setup.py
+++ gpt_config/openai_setup.py
@@ -1,20 +1,12 @@
+# gpt_config/openai_setup.py
+import openai
 import streamlit as st
 
 def initialize_openai():
-    # Verifica si 'openai' está en st.secrets
-    if "openai" in st.secrets:
-        # Verifica si 'api_key' está en st.secrets['openai']
-        if "api_key" in st.secrets["openai"]:
-            OPENAI_API_KEY = st.secrets["openai"]["api_key"]
-            st.write("API Key de OpenAI cargada correctamente.")
-            return OPENAI_API_KEY
-        else:
-            st.error("La clave 'api_key' no se encuentra en st.secrets['openai'].")
-            # Imprime las claves disponibles en st.secrets['openai']
-            st.write("Claves disponibles en 'openai':", list(st.secrets["openai"].keys()))
-    else:
-        st.error("La clave 'openai' no se encuentra en st.secrets.")
-        # Imprime las claves disponibles en st.secrets
-        st.write("Claves disponibles:", list(st.secrets.keys()))
-    
-    return None
+    """Inicializa OpenAI y obtiene la clave API de Streamlit Secrets."""
+    OPENAI_API_KEY = st.secrets["OPENAI_API_KEY"]
+    if not OPENAI_API_KEY:
+        st.error("Please add your OpenAI API key to the Streamlit secrets.toml file.")
+        st.stop()
+    openai.api_key = OPENAI_API_KEY
+    return openai.OpenAI()
