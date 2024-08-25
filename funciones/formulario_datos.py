c5395479769e6cbd06d8674e1d9dfa7a784470e8
Francisco Curiel de Icaza <ciclosporina2@hotmail.com>
2024-08-25 10:16:13 -0400

    Update formulario_datos.py

--- funciones/formulario_datos.py
+++ funciones/formulario_datos.py
@@ -1,1 +1,19 @@
+# funciones/formulario_datos.py
+import streamlit as st
 
+def formulario_datos():
+    st.header("Datos Biométricos del Paciente")
+
+    edad = st.number_input("Edad:", min_value=0, max_value=120, step=1)
+    peso = st.number_input("Peso (kg):", min_value=0.0, max_value=300.0, step=0.1)
+    talla = st.number_input("Talla (cm):", min_value=0.0, max_value=250.0, step=0.1)
+    sintomas = st.text_area("Síntomas:")
+
+    datos = {
+        "edad": edad,
+        "peso": peso,
+        "talla": talla,
+        "sintomas": sintomas
+    }
+
+    return datos
